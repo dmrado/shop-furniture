429ca6f8296408a6d3c07e426e01fc711065134a
Daniil <149520732+Turlush@users.noreply.github.com>
2024-11-13 21:03:16 +0300

    first commit

--- src/components/site/MainPage.tsx
+++ src/components/site/MainPage.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Image from "next/image";
+import Decoro from './img/Decoro.svg'
 
 const MainPage = () => {
     return (
-        <div className="">
-            Верстай здесь!
+        <div className="grid flex-col ">
+            <header className="grid flex-row items-center content-around">
+
+                <div className="">
+                    <Image src={Decoro} alt="" width="135" height="35"/>
+                </div>
+
+                <div className="">
+                    Navigation bar
+                </div>
+
+                <div className="">
+                    Call Heart Cart
+                </div>
+
+                <div className="">
+                    Profile
+                </div>
+
+            </header>
         </div>
     )
 }
