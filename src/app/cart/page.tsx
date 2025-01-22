939ef4c980eb70a34b93bd7dad40bdf3df0f5e05
dmrado <8838525@gmail.com>
2025-01-22 21:00:21 +0300

    <CartTotalAmount/> on cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,7 @@
 import CartTotal from '@/components/cart/CartTotal'
 import Cart from '@/components/cart/Cart'
 import { getServerSession } from 'next-auth'
+import CartTotalAmount from "@/components/cart/CartTotalAmount";
 
 const CartPage = async () => {
     const session = await getServerSession()
@@ -12,6 +13,7 @@ const CartPage = async () => {
 
     return (
         <div>
+            <CartTotalAmount/>
             <CartTotal />
             <Cart />
         </div>
