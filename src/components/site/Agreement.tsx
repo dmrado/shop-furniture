9716b49023e5a44da3f936dcbb3b92391bf03a60
dmrado <8838525@gmail.com>
2025-03-22 11:16:36 +0300

    Transition in Disclosure was killed but transition was added

--- src/components/site/Agreement.tsx
+++ src/components/site/Agreement.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React, { useEffect, useRef } from 'react'
-import { Disclosure, Transition } from '@headlessui/react'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
 import ConfidentialPolicy from '@/components/site/ConfidentialPolicy'
 import { isAgreedFromModelAction, updateUserAgreementAction } from '@/actions/userActions'
 
@@ -46,56 +47,45 @@ const Agreement = ({ setAgreed, agreed, userId }: Props) => {
         <Disclosure>
             {({ open }) => (
                 <>
-                    <Disclosure.Button
+                    <DisclosureButton
                         ref={disclosureButtonRef}
                         type="button"
-                        className="flex w-full justify-between rounded-lg bg-gray-50 px-4 py-3 text-left text-sm font-medium hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-500 focus-visible:ring-opacity-75">
+                        className="flex w-full justify-between rounded-lg bg-gray-100 px-4 py-3 text-left text-sm font-medium hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-indigo-500 focus-visible:ring-opacity-75">
                         <div className="w-full">
                             <h1 className="text-center w-full text-lg font-bold text-gray-700">
                                 Ознакомиться с политикой обработки персональных данных
                             </h1>
                         </div>
-                        <ChevronDownIcon
-                            className={`${
-                                open ? 'rotate-180 transform' : ''
-                            } h-5 w-5 text-gray-500 transition-transform`}
-                        />
-                    </Disclosure.Button>
+                        <ChevronDownIcon className={clsx('w-5', open && 'rotate-180')} />
+                    </DisclosureButton>
 
-                    <Transition
-                        show={open}
-                        enter="transition duration-100 ease-out"
-                        enterFrom="transform scale-95 opacity-0"
-                        enterTo="transform scale-100 opacity-100"
-                        leave="transition duration-75 ease-out"
-                        leaveFrom="transform scale-100 opacity-100"
-                        leaveTo="transform scale-95 opacity-0"
+                    <DisclosurePanel
+                        transition
+                        className="origin-top transition duration-300 ease-out data-[closed]:-translate-y-6 data-[closed]:opacity-0 px-4 pt-4 pb-2 text-sm text-gray-500"
                     >
-                        <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
 
-                            {/* Здесь текст политики */}
-                            <ConfidentialPolicy/>
+                        {/* Здесь текст политики */}
+                        <ConfidentialPolicy/>
 
-                            {/* Agreement checkbox */}
-                            <div className="flex items-start mt-6">
-                                <div className="flex items-center h-5">
-                                    <input
-                                        id="agreement"
-                                        type="checkbox"
-                                        checked={agreed}
-                                        onChange={handleCheckboxChange}
-                                        className="w-4 h-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
-                                    />
-                                </div>
-                                <label htmlFor="agreement" className="ml-3 text-sm text-gray-700">
+                        {/* Agreement checkbox */}
+                        <div className="flex items-start mt-6">
+                            <div className="flex items-center h-5">
+                                <input
+                                    id="agreement"
+                                    type="checkbox"
+                                    checked={agreed}
+                                    onChange={handleCheckboxChange}
+                                    className="w-4 h-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+                                />
+                            </div>
+                            <label htmlFor="agreement" className="ml-3 text-sm text-gray-700">
                                     Я согласен на обработку персональных данных в соответствии с
                                     настоящей политикой согласно Федеральному закону от 27.07.2006 №
                                     152-ФЗ «О персональных данных».
-                                </label>
-                            </div>
+                            </label>
+                        </div>
 
-                        </Disclosure.Panel>
-                    </Transition>
+                    </DisclosurePanel>
                 </>
             )}
         </Disclosure>
