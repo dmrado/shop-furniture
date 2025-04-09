a002eb3297d4982163e45d425dc81468c60d279d
dmrado <8838525@gmail.com>
2025-04-09 21:02:42 +0300

    Финализирован categories name в layout<Title/>

--- src/components/site/Title.tsx
+++ src/components/site/Title.tsx
@@ -2,19 +2,11 @@ import React from 'react'
 import { getSubCategoryName } from '@/actions/categoryActions'
 
 const Title = async ({ params }) => {
-    console.log('=========== params from Title', params)
-
     const subCategorySlug = params.slug
-
     const { subCategoryName: name } = await getSubCategoryName(subCategorySlug)
 
-    // const { subCategoryName } = await getSubCategoryId(params.name)
-    console.log('=========== subCategory name from Title', name)
-
     return <>
-        {/* Заголовок страницы */}
         <h1 className="text-3xl font-medium text-center mb-8">{name}</h1>
     </>
 }
-
 export default Title
