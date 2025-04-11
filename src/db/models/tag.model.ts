8a9ba72e5c588bc6be9e8b8b40b1e58c7d96ece8
dmrado <8838525@gmail.com>
2025-04-11 21:48:44 +0300

    Update tag.model.ts

--- src/db/models/tag.model.ts
+++ src/db/models/tag.model.ts
@@ -1,12 +1,12 @@
-import { sequelize } from "../connection";
+import { sequelize } from '../connection'
 import {
-  Model,
-  DataTypes,
-  InferAttributes,
-  InferCreationAttributes,
-  CreationOptional,
-} from "sequelize";
-import { ProductModel } from "./product.model";
+    Model,
+    DataTypes,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+} from 'sequelize'
+import { ProductModel } from './product.model'
 
 export interface Tag extends InferAttributes<TagModel> {}
 
@@ -14,74 +14,74 @@ export class TagModel extends Model<
   InferAttributes<TagModel>,
   InferCreationAttributes<TagModel>
 > {
-  declare id?: CreationOptional<number>;
-  declare parentId: number | null;
-  declare name: string;
-  declare slug: string;
-  declare products?: ProductModel[]; // Связь с ProductModel (many-to-many)
+    declare id?: CreationOptional<number>
+    declare parentId: number | null
+    declare name: string
+    declare slug: string
+    declare products?: ProductModel[] // Связь с ProductModel (many-to-many)
 }
 
 // Инициализация модели
 
-  TagModel.init(
+TagModel.init(
     {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      parentId: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-          model: "tags", // Ссылка на саму себя для организации иерархии
-          key: "id",
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
         },
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      slug: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
+        parentId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: 'tags', // Ссылка на саму себя для организации иерархии
+                key: 'id',
+            },
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
     },
     {
-      sequelize,
-      tableName: "tags",
+        sequelize,
+        tableName: 'tags',
     }
-  );
+)
 
 //todo перенесено в index
 // Настройка ассоциаций
 export const setupTagAssociations = (models: any) => {
-  const { TagModel, ProductModel } = models;
+    const { TagModel, ProductModel } = models
 
-  // Связь тега с родительским тегом (для организации иерархии)
-  TagModel.belongsTo(TagModel, {
-    foreignKey: "parentId",
-    as: "parent",
-  });
+    // Связь тега с родительским тегом (для организации иерархии)
+    TagModel.belongsTo(TagModel, {
+        foreignKey: 'parentId',
+        as: 'parent',
+    })
 
-  TagModel.hasMany(TagModel, {
-    foreignKey: "parentId",
-    as: "children",
-  });
+    TagModel.hasMany(TagModel, {
+        foreignKey: 'parentId',
+        as: 'children',
+    })
 
-  // Связь многие-ко-многим с продуктами
-  TagModel.belongsToMany(ProductModel, {
-    through: "product_tags", // Промежуточная таблица
-    foreignKey: "tagId",
-    otherKey: "productId",
-    as: "products",
-  });
+    // Связь многие-ко-многим с продуктами
+    TagModel.belongsToMany(ProductModel, {
+        through: 'product_tags', // Промежуточная таблица
+        foreignKey: 'tagId',
+        otherKey: 'productId',
+        as: 'products',
+    })
 
-  ProductModel.belongsToMany(TagModel, {
-    through: "product_tags", // Промежуточная таблица
-    foreignKey: "productId",
-    otherKey: "tagId",
-    as: "tags",
-  });
-};
+    ProductModel.belongsToMany(TagModel, {
+        through: 'product_tags', // Промежуточная таблица
+        foreignKey: 'productId',
+        otherKey: 'tagId',
+        as: 'tags',
+    })
+}
