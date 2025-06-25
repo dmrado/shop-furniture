5f0558bfcb121b739c5e080640d33d8b5fc0da06
dmrado <8838525@gmail.com>
2025-06-25 22:12:55 +0300

    Update next.config.mjs
    avatars.yandex.net

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
                 pathname:  '/**',
                 search: '',
             },
+            {
+                protocol: 'https',
+                hostname: 'avatars.yandex.net', // <--- ДОБАВЬТЕ ЭТУ ЗАПИСЬ
+                port: '',
+                pathname:  '/**',
+                search: '', // Необязательно
+            },
         ],
     },
 }
