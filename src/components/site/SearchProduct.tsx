a4738eacc5762fff7dc897c673a0e2719c72f8f9
Vladimir Kromm <vkromm@gmail.com>
2025-08-11 19:18:08 +0200

    search product live coding

--- src/components/site/SearchProduct.tsx
+++ src/components/site/SearchProduct.tsx
@@ -37,87 +37,32 @@ const SearchProduct = ({
         setArticulSearchTerm(searchParams.get(articulQueryKey) || '')
     }, [ searchParams, queryKey, articulQueryKey ])
 
-    // Объединенный useEffect URL обновляется только один раз, когда пользователь перестаёт вводить текст в любом из полей заменяет два нижних отдельных но он вообще ломает пагинацию строкой current.delete('page')
-    // useEffect(() => {
-    //     const handler = setTimeout(() => {
-    //         const current = new URLSearchParams(searchParams.toString())
-    //
-    //         // Обновляем параметр поиска по названию
-    //         if (nameSearchTerm) {
-    //             current.set(queryKey, nameSearchTerm)
-    //         } else {
-    //             current.delete(queryKey)
-    //         }
-    //
-    //         // Обновляем параметр поиска по артикулу
-    //         if (articulSearchTerm) {
-    //             current.set(articulQueryKey, articulSearchTerm)
-    //         } else {
-    //             current.delete(articulQueryKey)
-    //         }
-    //
-    //         // Сбрасываем на первую страницу при любом изменении поиска
-    //         current.delete('page')
-    //
-    //         router.push(`${pathname}?${current.toString()}`)
-    //     }, debounceTime)
-    //
-    //     return () => {
-    //         clearTimeout(handler)
-    //     }
-    // }, [ nameSearchTerm, articulSearchTerm, debounceTime, router, pathname, searchParams, queryKey, articulQueryKey ])
-
-    // Эффект для debounce-поиска по названию
-    useEffect(() => {
-        console.log('useEffect 1')
-        const handler = setTimeout(() => {
-            const current = new URLSearchParams(searchParams.toString())
-            if (nameSearchTerm) {
-                current.set(queryKey, nameSearchTerm)
-            } else {
-                current.delete(queryKey)
-            }
-            current.delete('page') // Сбрасываем на первую страницу при любом изменении поиска именно здесь оставляем
-            router.push(`${pathname}?${current.toString()}`)
-        }, debounceTime)
-
-        return () => {
-            clearTimeout(handler)
-        }
-    }, [ nameSearchTerm, debounceTime, router, pathname, searchParams, queryKey ])
-
-    // Эффект для debounce-поиска по артикулу
-    useEffect(() => {
-        console.log('useEffect 2')
-        const handler = setTimeout(() => {
-            const current = new URLSearchParams(searchParams.toString())
-            if (articulSearchTerm) {
-                current.set(articulQueryKey, articulSearchTerm)
-            } else {
-                current.delete(articulQueryKey)
-            }
-            //current.delete('page') // fixme Сбрасываем на первую страницу при любом изменении поиска надо ли именно из за это строки пагинация перелетает на первую страницу все время?
-            router.push(`${pathname}?${current.toString()}`)
-        }, debounceTime)
-
-        return () => {
-            clearTimeout(handler)
-        }
-    }, [ articulSearchTerm, debounceTime, router, pathname, searchParams, articulQueryKey ])
-
-    // Обработчик для кнопки "Очистить"
-    const handleClearSearch = () => {
-        setNameSearchTerm('')
-        setArticulSearchTerm('')
+    const handleSearchChange = (queryKey:string, queryValue: string) => {
         const current = new URLSearchParams(searchParams.toString())
-        current.delete(queryKey)
-        current.delete(articulQueryKey)
+        if (queryValue) {
+            current.set(queryKey, queryValue)
+        } else {
+            current.delete(queryKey)
+        }
         current.delete('page')
         router.push(`${pathname}?${current.toString()}`)
     }
 
-    // Проверяем, есть ли активные поисковые запросы (для отображения кнопки "Очистить")
-    const hasActiveSearch = nameSearchTerm !== '' || articulSearchTerm !== ''
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            handleSearchChange(queryKey, nameSearchTerm)
+        }, debounceTime)
+
+        return () => clearTimeout(handler)
+    }, [ nameSearchTerm ])
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            handleSearchChange(articulQueryKey, articulSearchTerm)
+        }, debounceTime)
+
+        return () => clearTimeout(handler)
+    }, [ articulSearchTerm ])
 
     return (
         <div className="flex flex-col sm:flex-row items-center gap-4 mb-4">
